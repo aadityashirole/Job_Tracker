db00823605b671b6536521492028e7709b6e3291
aadityashirole <aadityashirole@gmail.com>
2026-06-25 18:04:58 +0530

    Deployment Done

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api"
+const API_URL = "https://job-tracker-qyzl.onrender.com/api"
 
 export async function registerUser(name, email, password) {
     const res = await fetch(`${API_URL}/auth/register`, {
