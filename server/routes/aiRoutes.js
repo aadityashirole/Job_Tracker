d2d21c791a0282895b9431f842a59e8da14b9e22
aadityashirole <aadityashirole@gmail.com>
2026-07-18 19:42:00 +0530

    changes in InterviewPrep AI Feature

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const Question = require("../models/Question")
+const Question = require("../models/Questions")
 
 router.post("/jd-analyzer", async (req, res) => {
     try {
